74096078d0c182c1ff594995aa07c99c312c36fc
zhangkaida <zhangkda@foxmail.com>
2017-08-04 20:25:32 +0800

    en

---
+++ js/index.js
@@ -0,0 +1,74 @@
+var app = angular.module("app",["ngRoute"]);
+app.controller("ctrl",function($scope,$http){
+    $scope.fun = function(){
+    	var swiper = new Swiper('.swiper-container', {             
+   	    pagination: '.swiper-pagination',
+   	    autoplay:5000,
+   	    paginationClickable: true,
+   	    autoplayDisableOnInteraction:false,            
+   	    });
+    }
+    $scope.fun2 = function(){
+    	$(".b-ul li").click(function(){
+   	  	 $(this).addClass("on").siblings().removeClass("on") 
+   	  })
+    }
+    $scope.fun3 = function(){
+    	$("#left").click(function(){
+   	  	if ($(".b-img>ul").css("left")=="-960px") {
+   	  	  $(".b-img>ul").animate({"left":"0"})   	  		
+   	  	}else{
+   	  	  $(".b-img>ul").animate({"left":"-960px"})   	  		
+   	  		
+   	  	}
+   	  	
+   	  })
+   	  $("#right").click(function(){
+   	  	if ($(".b-img>ul").css("left")=="-960px") {
+   	  	  $(".b-img>ul").animate({"left":"0"})   	  		
+   	  	}else{
+   	  	  $(".b-img>ul").animate({"left":"-960px"})   	  		
+   	  		
+   	  	}
+   	  	
+   	  })
+    }
+})
+app.config(["$routeProvider",function($routeProvider){
+    $routeProvider
+    .when("/index2",{templateUrl:"index2.html"})
+    .when("/index3",{templateUrl:"index3.html"})
+    .when("/successful",{templateUrl:"successful.html"})
+    .when("/media",{templateUrl:"media.html"})
+    .when("/dream",{templateUrl:"dream.html"})
+    .when("/about",{templateUrl:"about_us.html"})
+    .when("/partners",{templateUrl:"partners.html"})
+    .when("/contact",{templateUrl:"contact_us.html"})
+    .when("/site_1",{templateUrl:"server/site_1.html"})
+    .when("/site_2",{templateUrl:"server/site_2.html"})
+    .when("/site_3",{templateUrl:"server/site_3.html"})
+    .when("/site_4",{templateUrl:"server/site_4.html"})
+    .when("/site_5",{templateUrl:"server/site_5.html"})
+    .when("/site_6",{templateUrl:"server/site_6.html"})
+    .when("/file1",{templateUrl:"dream/dfile_1.html"})
+    .when("/file2",{templateUrl:"dream/dfile_2.html"})
+    .when("/file3",{templateUrl:"dream/dfile_3.html"})
+    .when("/file4",{templateUrl:"dream/dfile_4.html"})
+    .when("/file5",{templateUrl:"dream/dfile_5.html"})
+    
+    .otherwise({
+        redirectTo:"index2"
+    })
+}]);
+$(function() {
+		$(window).scroll(function() {
+			if($(window).scrollTop() > 200)
+				$('#go_top').show();
+			else
+				$('#go_top').hide();
+		});
+		$('#go_top').click(function() {
+			$('html, body').animate({ scrollTop: 0 }, 1000);
+		});
+		console.log(22);
+})
